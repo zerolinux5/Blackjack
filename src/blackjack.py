319e7febc491e97d0f09f07d5ece2d561fc51f88
Jesus Magana <maganazero5@gmail.com>
2018-01-09 20:34:07 -0800

    feat: Add end game logic
    Dealer logic is to hit until over 17 if player didn't bust. Add end game logic to check numeric values

--- src/blackjack.py
+++ src/blackjack.py
@@ -4,6 +4,8 @@ import random
 #Constants
 MAGIC_SHUFFLE_NUMBER = 7
 END_GAME = 21
+DEALER_NUMER = 17
+ACE_DIFF = 10
 
 #Game deck, used to shuffle and keep track of cards
 class Deck(object):
@@ -160,10 +162,57 @@ def players_turn(human, game_deck):
                 # While we have aces to reduce
                 while ace > 0:
                     ace -= 1
-                    end_value -= 10
+                    end_value -= ACE_DIFF
                     if end_value < END_GAME:
                         break
     return end_value
+
+# Dealers logic, hit if under 17
+def dealers_turn(dealer, game_deck):
+    end_value = 0
+    ace = 0
+    value = None
+    cont = True
+
+    # Count player starting hand
+    for card in dealer.get_hand():
+        new_value = card.get_val()
+        if new_value == 1:
+            ace += 1
+            new_value = 11
+        end_value += new_value
+        if end_value >= DEALER_NUMER:
+            cont = False
+
+    # Keep hitting until player says end or is over
+    while cont:
+        new_card = game_deck.pop_card()
+        new_val = new_card.get_val()
+        if new_val == 1:
+            ace += 1
+            new_val = 11
+        end_value += new_val
+        dealer.hit(new_card)
+        if end_value >= DEALER_NUMER:
+            cont = False
+        if end_value > END_GAME:
+            # No aces so we broke
+            if ace <= 0:
+                cont = False
+            # While we have aces to reduce
+            while ace > 0:
+                ace -= 1
+                end_value -= ACE_DIFF
+                if end_value < END_GAME:
+                    if end_value < DEALER_NUMER:
+                        cont = True
+                    break
+    return end_value
+
+# Print the end game hands
+def print_end_hands(player, dealer):
+    print "Player end cards:" + str(player)
+    print "Dealer end cards:" + str(dealer)
 
 # Main loop
 def main():
@@ -178,9 +227,22 @@ def main():
     #2. Loop to allow player to keep hitting
     player_val = players_turn(human, game_deck)
 
-    #3. Logic for dealer
+    #3. Logic for dealer only if player didn't bust
+    if player_val <= END_GAME:
+        dealer_val = dealers_turn(dealer, game_deck)
+    # Player bust so set him to -1
+    else:
+        player_val = -1
+        # Since player bust, set dealer to 1 to win
+        dealer_val = 1
+
+    # Dealer bust
+    if dealer_val > END_GAME:
+        dealer_val = -1
 
     #4. Calulcate winner
+    print "Player won!" if player_val > dealer_val else "Dealer won!"
+    print_end_hands(human, dealer)
 
 #Call main if not imported
 if __name__ == "__main__":
